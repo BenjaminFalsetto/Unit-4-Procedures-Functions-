6b7fb24c59c4e2a3cca50f591889738163ccaf00
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-11-25 10:32:24 -0500

    Address Program

---
+++ AddressProgramBenF/AddressProgramBenF/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AddressProgramBenF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirst = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.lblMiddle = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblLast = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirst
+            // 
+            this.lblFirst.AutoSize = true;
+            this.lblFirst.Location = new System.Drawing.Point(51, 49);
+            this.lblFirst.Name = "lblFirst";
+            this.lblFirst.Size = new System.Drawing.Size(103, 13);
+            this.lblFirst.TabIndex = 0;
+            this.lblFirst.Text = "Enter your first name";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(52, 75);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Location = new System.Drawing.Point(192, 75);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(100, 20);
+            this.txtMiddleName.TabIndex = 3;
+            // 
+            // lblMiddle
+            // 
+            this.lblMiddle.AutoSize = true;
+            this.lblMiddle.Location = new System.Drawing.Point(184, 49);
+            this.lblMiddle.Name = "lblMiddle";
+            this.lblMiddle.Size = new System.Drawing.Size(117, 13);
+            this.lblMiddle.TabIndex = 2;
+            this.lblMiddle.Text = "Enter your middle name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(366, 75);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // lblLast
+            // 
+            this.lblLast.AutoSize = true;
+            this.lblLast.Location = new System.Drawing.Point(365, 49);
+            this.lblLast.Name = "lblLast";
+            this.lblLast.Size = new System.Drawing.Size(103, 13);
+            this.lblLast.TabIndex = 4;
+            this.lblLast.Text = "Enter your last name";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(214, 129);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 6;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 180);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.lblMiddle);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblLast);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblFirst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirst;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.Label lblMiddle;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblLast;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ AddressProgramBenF/AddressProgramBenF/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressProgramBenF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintName(String firstName, String middleName, String lastName)
+        {
+            MessageBox.Show("Your name is: " + firstName + " " + middleName + " " + lastName);
+        }
+
+        public void PrintName(String firstName, String lastName)
+        {
+            MessageBox.Show("Your name is " + firstName + " " + lastName);
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            String userFirstName, userMiddleName, userLastName;
+
+            userFirstName = this.txtFirstName.Text;
+            userMiddleName = this.txtMiddleName.Text;
+            userLastName = this.txtLastName.Text;
+            Console.WriteLine("User first name is " + userFirstName);
+
+            if (userFirstName == "")
+            {
+                MessageBox.Show("Please enter your first name.", "Full Name Program");
+            }
+            else if (userLastName == "")
+            {
+                MessageBox.Show("Please enter your last name.", "Full Name Program");
+            }
+            else if (userMiddleName == "")
+            {
+                PrintName(userFirstName, userLastName);
+            }
+            else
+            {
+                PrintName(userFirstName, userMiddleName, userLastName);
+            }
+        }
+    }
+}
