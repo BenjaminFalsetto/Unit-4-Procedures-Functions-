b4ab3cd8cfb3d358de91d8b0d86e56ed0699d1b3
Benjamin Falsetto <32547110+BenjaminFalsetto@users.noreply.github.com>
2018-11-26 10:49:53 -0500

    String Equality

---
+++ StringEqualityBenF/StringEqualityBenF/StringEqualityForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace StringEqualityBenF
+{
+    partial class frmStringEquality
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblExplain = new System.Windows.Forms.Label();
+            this.txtString1 = new System.Windows.Forms.TextBox();
+            this.txtString2 = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblExplain
+            // 
+            this.lblExplain.AutoSize = true;
+            this.lblExplain.Location = new System.Drawing.Point(41, 38);
+            this.lblExplain.Name = "lblExplain";
+            this.lblExplain.Size = new System.Drawing.Size(255, 26);
+            this.lblExplain.TabIndex = 0;
+            this.lblExplain.Text = "Please input text into the first and second text boxes.\r\nThen, press the chack bu" +
+    "tton.";
+            // 
+            // txtString1
+            // 
+            this.txtString1.Location = new System.Drawing.Point(44, 86);
+            this.txtString1.Name = "txtString1";
+            this.txtString1.Size = new System.Drawing.Size(100, 20);
+            this.txtString1.TabIndex = 1;
+            // 
+            // txtString2
+            // 
+            this.txtString2.Location = new System.Drawing.Point(193, 86);
+            this.txtString2.Name = "txtString2";
+            this.txtString2.Size = new System.Drawing.Size(100, 20);
+            this.txtString2.TabIndex = 2;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(130, 130);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // frmStringEquality
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 188);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtString2);
+            this.Controls.Add(this.txtString1);
+            this.Controls.Add(this.lblExplain);
+            this.Name = "frmStringEquality";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String Equality Benjamin Falsetto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblExplain;
+        private System.Windows.Forms.TextBox txtString1;
+        private System.Windows.Forms.TextBox txtString2;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ StringEqualityBenF/StringEqualityBenF/StringEqualityForm.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Created by: Benjami Falsetto
+ * Created on: 2018/11/26
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #35 - String Equality
+ * This program takes two strings and compares them to see if they're equal
+ * then the program tells the user whether the strings are equal or not
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringEqualityBenF
+{
+    public partial class frmStringEquality : Form
+    {
+        public frmStringEquality()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            bool ans;
+            string userString1, userString2;
+
+            //get the strings and assign them to their respective variables
+            userString1 = this.txtString1.Text;
+            userString2 = this.txtString2.Text;
+
+            //convert each string to lowercase
+            userString1.ToLower();
+            userString2.ToLower();
+
+            //call the compare function
+            ans = StringsEqual(userString1, userString2);
+
+            //see if the strings are equal to one another and show message boxes accordingly
+            if (ans == true)
+            {
+                MessageBox.Show("The strings are equal");
+            }
+            else
+            {
+                MessageBox.Show("The strings are not equal");
+            }
+        }
+
+        private bool StringsEqual(string string1, string string2)
+        {
+            //declare local variables
+            bool ans;
+
+            //check to see if the two strings are equal
+            if (string1 == string2)
+            {
+                ans = true;
+            }
+            else
+            {
+                ans = false;
+            }
+
+            //return the boolean variable
+            return ans;
+        }
+    }
+}
